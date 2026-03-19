c9908fcc819404360a6f51e79e9fcfb81ecd235b
Dianella Sy <dianellabsy@csu.fullerton.edu>
2026-03-19 06:34:47 -0700

    Edited Spacing in EventTicketingSystemApplication.java and Configured Database Connection in application.properties

--- src/main/java/com/example/event_ticketing_system/EventTicketingSystemApplication.java
+++ src/main/java/com/example/event_ticketing_system/EventTicketingSystemApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EventTicketingSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EventTicketingSystemApplication.class, args);
 	}
-
 }
